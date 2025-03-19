a75f395b19e29004dcf3174afd35c4e29b63f58a
G-H-Pulner <contami732@gmail.com>
2025-03-19 12:41:26 -0300

    simple_regression

--- LibraryML/LinearRegression.py
+++ LibraryML/LinearRegression.py
@@ -3,9 +3,26 @@ import seaborn as sns
 import numpy as np
 import random
 
-class SimpleLinearRegression:
+class simple_regression:
     """
+    Contem a regeressão linear utilizando 
+    cálculo 1, usando máximos e mínimos, 
+    juntamente com a versão utilizando o 
+    gradiente descendente.
+
+    ==========================================
+
     Versão cálculo 1: usando máximos e mínimos
+    
+    Parâmetros
+    ----------
+    x: variável dependente, a que explica y,
+       feature, atributo, característica
+    y: variável independente, a ser predita
+    
+    ==========================================
+
+    Versão com gradiente descendente:
 
     Parâmetros
     ----------
@@ -14,43 +31,50 @@ class SimpleLinearRegression:
     y: variável independente, a ser predita
     """
 
-    def linear_regression(self, x, y):
-        self.x = np.array(x)
-        self.y = np.array(y)
-        A = [
-            [sum(x ** 2), sum(x)],
-            [sum(x), len(x)]
-        ]
-        B = [sum(x * y),sum(y)]
-        return np.linalg.solve(A, B)
-    
     def split(self, x, y, test_size=0.3):
         self.x = x
         self.y = y
         dados = set(zip(x, y))
-        k = int(test_size * 10)
+        k = int(test_size * 100)
         test = set(random.sample(list(dados), k))
         train = dados - test
         return *list(zip(*train)), *list(zip(*test))
 
     def fit(self, x, y):
+        x = np.array(x)
+        y = np.array(y)
         self.x = x
         self.y = y
-        w, b = SimpleLinearRegression.linear_regression(x, y)
+        A = [
+            [sum(x ** 2), sum(x)],
+            [sum(x), len(x)]
+        ]
+        B = [sum(x * y),sum(y)]
+        w, b = np.linalg.solve(A, B)
         self.w = w
         self.b = b
-        self.line = SimpleLinearRegression.best_line(w, b)
+
+    def fit_gd(self, x, y, alpha=0.01, iter=200, w=0, b=0):
+        x = np.array(x)
+        y = np.array(y)
+        self.x = x
+        self.y = y
+        self.alpha = alpha
+        self.iter = iter
+        self.w = w
+        self.b = b
+        n = x.shape[0]
+        for _ in range(self.iter):
+            px = self.w * x + self.b # é o (p(x1), p(x2), ..., p(xn))
+            dw = np.dot(x, px - y) / n
+            db = np.sum(px - y) / n
+            # gradiente descendente
+            self.w -= alpha * dw
+            self.b -= alpha * db
 
     def predict(self, x):
-        return self.line(x)
-    
-    def best_line(self, w, b):
-        self.w = w
-        self.b = b
-        def line(self, x):
-            self.x = x
-            return w * x + b
-        return line
+        x = np.array(x)
+        return self.w * x + self.b
     
     def RMSE(self, x, y):
         # Root Mean Squared Error
@@ -60,42 +84,12 @@ class SimpleLinearRegression:
     def plot(self, name='output.png', points=200):
         t = np.linspace(min(self.x), max(self.x), points)
         ft = [self.predict(ti) for ti in t]
-        sns.scatterplot(x=self.x, y=self.y)
+        sns.scatterplot(x=self.x, y=self.y, alpha = 0.5)
         sns.lineplot(x=t, y=ft, color='red')
         plt.savefig(name)
         plt.show()
 
-class SimpleLinearRegressionWithGradientDescendent:
-    
-    def __init__(self, alpha=0.01, iter=200, w=0, b=0):
-        self.alpha = alpha
-        self.iter = iter
-        self.w = w
-        self.b = b
-
-    def fit(self, X, y):
-        alpha = self.alpha
-        n = X.shape[0]
-        for _ in range(self.iter):
-            px = self.w * X + self.b # é o (p(x1), p(x2), ..., p(xn))
-            dw = np.dot(X, px - y) / n
-            db = np.sum(px - y) / n
-            # gradiente descendente
-            self.w -= alpha * dw
-            self.b -= alpha * db
-
-    def predict(self, x):
-        return self.w * x + self.b
-    
-    def best_line(self, w, b):
-        self.w = w
-        self.b = b
-        def line(self, x):
-            self.x = x
-            return w * x + b
-        return line
-
-class LinearRegressionWithGradientDescendent:
+class multiple_regression:
 
     def __init__(self, alpha=0.01, iter=200):
         self.alpha = alpha # learning rate
